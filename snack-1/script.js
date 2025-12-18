33d0a53700984b6038ddaf924b8499687063814d
marcotristani <marco.tristani@hotmail.it>
2025-12-18 13:29:41 +0100

    flat: snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,7 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach(nameSingle => console.log(nameSingle));
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
