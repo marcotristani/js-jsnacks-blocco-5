4abcbd19a51e6d6a29e7eedb633a63be349a7426
marcotristani <marco.tristani@hotmail.it>
2025-12-18 13:37:38 +0100

    flat: snack4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,8 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+
+const authorPosts = posts.map(post => post.author);
+console.log(authorPosts);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
